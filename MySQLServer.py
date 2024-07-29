62f4bfd44fb70b3203e8127a551938af0d5076e1
boa20 <kwesikyeremateng5@gmail.com>
2024-07-29 00:15:07 +0000

    Added Python code to create a database

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    user = "root",
+    password = "Icui4cu@",
+    host = "localhost",
+    database = "alx_book_store"
+)
+
+
+
+
+
+connection.close()
