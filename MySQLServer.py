7f5c8876c9dc8209aa88ee17ba844c015111961f
boa20 <kwesikyeremateng5@gmail.com>
2024-07-29 00:18:26 +0000

    Updated Python code to create a database

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,11 +3,12 @@ import mysql.connector
 connection = mysql.connector.connect(
     user = "root",
     password = "Icui4cu@",
-    host = "localhost",
-    database = "alx_book_store"
+    host = "localhost"
 )
 
+mycursor = connection.cursor()
 
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
 
 
