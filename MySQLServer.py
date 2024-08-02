34137d546097a06262089921f7947bdd12887cf1
boa20 <kwesikyeremateng5@gmail.com>
2024-08-02 16:18:34 +0000

    Updated the MySQLServer python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,20 +1,20 @@
 import mysql.connector
 
-connection = mysql.connector.connect(
-    user = "root",
-    password = "Icui4cu@",
-    host = "localhost"
-)
+try:
+    connection = mysql.connector.connect(
+        user = "root",
+        password = "Icui4cu@",
+        host = "localhost"
+    )
 
-mycursor = connection.cursor()
+    mycursor = connection.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-if mysql.connector.Error == True:
-    print("Failed to connect to database")
-
-else:
     print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as err:
+    print("Failed to connect to database")
 
 
 mycursor.close()
