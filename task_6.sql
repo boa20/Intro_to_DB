dcfae74f74ccf80093744f87dbe0f2bee45d3cb9
boa20 <kwesikyeremateng5@gmail.com>
2024-08-02 19:31:59 +0000

    Added script that inserts multiple rows in the 'customer' table

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+    VALUES ROW ('2', 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+           ROW ('3', 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+           ROW ('4', 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
+
